1a6e77ab9a73f38898b5fb580ab79705c39d3aa7
TracyAntonia <aantonjuguna@gmail.com>
2023-06-23 02:14:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-function addingEventListener() {
+const btn = document.getElementById("button")
+
+ function clickAlert(){
+    alert("I was clicked!");
 }
+
+function addingEventListener() {
+    alert("I was clicked!");
+    btn.addEventListener("click", clickAlert);
+}
+addingEventListener()
+
+
+
+
+// const button = document.getElementById("button")
+
+// function clickAlert(){
+//     alert("I was clicked");
+// }
+
+
+// function addingEventListener() {
+//     input.addingEventListener("click", clickAlert);
+// }
+
+// function clickAlert(){
+//     alert("I was clicked!");
+// }
+// addingEventListener()
